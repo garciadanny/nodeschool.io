6a229f600d440c422f4038ff5bcccbe2f4e56108
garciadanny <dannygarcia.me@gmail.com>
2015-03-27 10:39:24 -0600

    refactor juggling async

--- learnyounode/juggling_async.js
+++ learnyounode/juggling_async.js
@@ -7,20 +7,24 @@ urls.forEach(function(url) {
   var urlOrder = urls.indexOf(url);
 
   http.get(url, function(response) {
-    var responseContents = '';
-
     response.setEncoding('utf8');
+
+    var responseContents = '';
     response.on('data', function(chunk) {
       responseContents += chunk
     });
+
     response.on('end', function() {
       results.splice(urlOrder, 0, responseContents);
-      if(results.length === 3) {
-        results.forEach(function(content) {
-          console.log(content);
-        });
-      }
+      printResultsWhenComplete()
     });
   });
 });
 
+function printResultsWhenComplete() {
+  if(results.length === 3) {
+    results.forEach(function(content) {
+      console.log(content);
+    });
+  }
+}
